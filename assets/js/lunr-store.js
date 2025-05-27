7102641388421e2f1feae5336c982deab181221b
camp5456 <camp5456@users.noreply.github.com>
2025-05-27 20:36:09 +0000

    deploy: 3f7158c41f0865d9d4bd0cde30a48a6c2b050303

---
+++ assets/js/lunr-store.js
@@ -0,0 +1,17 @@
+var store = [{  
+"title": "original arches","date": "unknown","creator": "unknown","description": "Original cast stone arches by Alexander Calder that adorned Caltech’s Throop Hall before earthquake.","subject": "architecture","location": "Throop Hall", 
+"id": "almost-a-lost-art-1.html"
+
+},{  
+"title": "fallem arches","date": "circa 1974","creator": "unknown","description": "A languishing sculpture, circa 1974. The figure represents Imagination and is winged, 'exulting in yet unexplored possibilities,' in the words of the artist. For the moment, the outlook was dim.","subject": "architecture","location": "Throop Hall", 
+"id": "almost-a-lost-art-2.html"
+
+},{  
+"title": "restoring arches","date": "1986","creator": "unknown","description": "Remounting the Calder arches, 1986.","subject": "architecture","location": "Throop Hall", 
+"id": "almost-a-lost-art-3.html"
+
+},{  
+"title": "new home for arches","date": "1986","creator": "unknown","description": "The Calder arches in their new setting, Beckman Laboratory of Chemical Synthesis.","subject": "architecture","location": "Beckman Laboratory of Chemical Synthesis", 
+"id": "almost-a-lost-art-4.html"
+
+}];
